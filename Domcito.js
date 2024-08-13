e41595593689d95462e500686a30f51dc57565eb
Rodrygo00 <rodrigomelarabarrera@gmail.com>
2024-08-13 08:39:38 -0600

    Add files via upload

---
+++ Domcito.js
@@ -0,0 +1,10 @@
+
+function calcular()
+{
+    let nombre=document.getElementById('name1').value;
+    let edad=parseInt(document.getElementById('edad1').value);
+    let aumento;
+    aumento=edad+10;
+    alert("Su edad aumentada es "+aumento);
+    
+}
